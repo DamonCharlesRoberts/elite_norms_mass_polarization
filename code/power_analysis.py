6dcfb1901a8a22198222b2d6b999cae5de8c88db
Damon C. Roberts <damon.roberts-1@colorado.edu>
2021-12-04 10:12:21 -0700

    updated power_analysis.py

--- code/power_analysis.py
+++ code/power_analysis.py
@@ -6,12 +6,20 @@
 	#* Updated by: dcr
 # Setup:
 	#* Load libraries
+import os
 from statsmodels.stats.power import TTestIndPower
 from matplotlib import pyplot as plt
+import seaborn as sns
+from numpy import arange
 from numpy import array
+	#* Working Directory
+os.chdir('/home/damoncroberts/Dropbox/current_projects/dcr_elite_norms_mass_polarization')
+	#* Set plot theme
+sns.set_theme(style = 'whitegrid')
+sns.color_palette('Greys_r')
 # Power analysis:
-effect_sizes = array([0.2, 0.5, 0.95])
-sample_sizes = array(range(5, 200))
+effect_sizes = list(arange(0.1, 5, .5))
+sample_sizes = array(arange(5, 200))
 analysis = TTestIndPower()
 analysis.plot_power(dep_var ='nobs', nobs=sample_sizes, effect_size = effect_sizes)
-plt.savefig('/Users/damonroberts/Dropbox/current_projects/dcr_elite_norms_mass_polarization/figures/power_analysis.jpeg')
+plt.savefig('figures/power_analysis.jpeg')
